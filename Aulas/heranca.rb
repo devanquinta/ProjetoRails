71df56bf06fbfac05b0effb32492562db55e1457
Vander <vander2014quintanilha@gmail.com>
2019-08-13 12:50:39 -0300

    Dia 13 de agosto 12:30

--- Aulas/heranca.rb
+++ Aulas/heranca.rb
@@ -13,7 +13,7 @@ end
 class Pessoajuridica < Pessoa
     attr_accessor:name, :cnpj
     def pagar_fornecedor
-        puts "Pagando fornecedor..."
+        "Pagando fornecedor..."
     end
 end
 
@@ -31,7 +31,13 @@ pessoa2 = PessoaFisica.new
 pessoa2.nome = "marcos"
 pessoa2.email = "marcos@hotmail.com"
 pessoa2.cpf = "11506914789"
-###############
+
+
+#getter
+puts pessoa2.nome 
+puts pessoa2.email
+puts pessoa2.cpf
+
 puts pessoa2.falar("Como vai")
 ln
 
@@ -40,6 +46,10 @@ pessoa3 = Pessoajuridica.new
 pessoa3.nome = "videos de TI"
 pessoa3.email = "close@tvt.com.br"
 pessoa3.cnpj = "45487876331-0003"
+#getter
+puts pessoa3.nome 
+puts pessoa3.email 
+puts pessoa3.cnpj 
 
 puts pessoa3.pagar_fornecedor
 ln
